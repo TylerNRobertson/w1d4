a850348bb8da5da1aafd4eb506fbc7603925dda0
Tyler Robertson <tylerr@tnrdesign.net>
2017-10-05 16:38:00 +0000

    Added waldo index and refactored the for loop

---
+++ waldo.js
@@ -0,0 +1,14 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(names, found) {
+  names.forEach(function(name, index){
+    if (name === "Waldo") {
+      found(index);   // execute callback
+    }
+  });
+}
+
+function actionWhenFound(index) {
+  console.log("Waldo is at index " + index);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
